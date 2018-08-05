6f6fd136cf07e8dba14020fa06488cf7244380bc
kepuvv <renard.ru@gmail.com>
2018-08-05 18:12:58 +0300

    add IBM_path

--- app.bat
+++ app.bat
@@ -2,6 +2,7 @@
 
 SET db="br_portal.nsf"
 SET path_to_db="E:\IBM\Domino\ReferentBR3\"
+SET IBM_path="C:\IBM\Domino\"
 
 REM Check if Domino Services still running
 SET services[0]=
@@ -19,18 +20,18 @@ SET services[2]=
 REM Start maintenance
 
 REM Start fixup
-echo Starting fixup on database %db%...
-C:\IBM\Domino\nfixup.exe -F -J %path_to_db%%db%
-echo Fixup on database %db% completed.
+echo Starting fixup on database "%db%"...
+"%IBM_path%"nfixup.exe -F -J "%path_to_db%""%db%"
+echo Fixup on database "%db%" completed.
 
 REM Start updall
 echo Starting updall on database %db%...
-C:\IBM\Domino\nupdall.exe -X %path_to_db%%db%
-echo Updall on database %db% completed.
+"%IBM_path%"nupdall.exe -X "%path_to_db%""%db%"
+echo Updall on database "%db%" completed.
 
 REM Start compact
 echo Starting compact on database %db%...
-C:\IBM\Domino\ncompact.exe -b %path_to_db%%db%
-echo Compact on database %db% completed.
+"%IBM_path%"ncompact.exe -b "%path_to_db%""%db%"
+echo Compact on database "%db%" completed.
 
 pause
