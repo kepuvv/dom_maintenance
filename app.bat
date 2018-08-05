6bda00cc062ebd1d4289a70e048923cd9a9853af
kepuvv <renard.ru@gmail.com>
2018-08-05 12:44:57 +0300

    add compact. finish

--- app.bat
+++ app.bat
@@ -4,16 +4,24 @@ SET db="br_portal.nsf"
 SET path_to_db="E:\IBM\Domino\Referentbr3\"
 
 REM Start fixup
-echo Starting fixup on database %db%
+echo Starting fixup on database %db%...
 
 C:\IBM\Domino\nfixup.exe -F -J %path_to_db%%db%
 
 echo Fixup on database %db% completed.
 
 REM Start updall
-echo Starting updall on database %db%
+echo Starting updall on database %db%...
 
 C:\IBM\Domino\nupdall.exe -X %path_to_db%%db%
 
-echo Updall on database %db% completed at %time%
+echo Updall on database %db% completed.
 
+REM Start compact
+echo Starting compact on database %db%...
+
+C:\IBM\Domino\ncompact.exe -b %path_to_db%%db%
+
+echo Compact on database %db% completed.
+
+pause
